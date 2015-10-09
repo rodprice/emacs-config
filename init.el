71518933971ff924536c21bfdaf28a8c9c3bf4d4
Rodney Price <rodprice@raytheon.com>
2015-10-09 12:13:13 -0600

    Add hi-lock package, set error bell

--- init.el
+++ init.el
@@ -6,12 +6,18 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Load some site-specific configuration before anything else
 
-(require 'functions)
-(defvar dotfiles-dir (file-name-directory
-                      (or (buffer-file-name) load-file-name))
+(defvar dotfiles-dir
+  (file-name-directory
+   (or (buffer-file-name) load-file-name))
   "The directory where the user's configuration files are kept.")
 
-(load-site-file "-preload")
+;; Set up load paths within .emacs.d
+(add-to-list 'load-path (concat dotfiles-dir "init/"))
+(add-to-list 'load-path (concat dotfiles-dir "mode/"))
+(add-to-list 'load-path (concat dotfiles-dir "site/"))
+
+;; See README in ~/.emacs.d/site/
+(load (concat system-name "-preload") 'noerror)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Make emacs run the server so emacsclientw can connect
@@ -34,6 +40,14 @@
   (package-install 'use-package))
 (require 'use-package)
 
+;; Preferred setup according to https://github.com/jwiegley/use-package
+;;(eval-when-compile
+;;  (require 'use-package))
+;;(require 'diminish)  ; if you use :diminish
+;;(require 'bind-key)  ; if you use any :bind variant
+
+(setq use-package-verbose t)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Load packages and configure them
 
@@ -47,17 +61,39 @@
   :config
   (load-theme 'hc-zenburn t))
 
+;; Needs setup (external perl install, perl code for ack)
 (use-package ack
-  :ensure t)
-	     
+  :disabled t)
+
+;; Highlight all regexps in a buffer
+(use-package hi-lock
+  :ensure t
+  :bind (("M-o r" . highlight-regexp)
+         ("M-o u" . unhighlight-regexp))
+  :config
+  (setq hi-lock-auto-select-face t)
+  (setq hi-lock-face-defaults '("hi-black-b" "hi-pink")))
+
+;; Placeholder for what looks like a great HTML, CSS, JavaScript dev package
+;; See also https://truongtx.me/2014/02/23/set-up-javascript-development-environment-in-emacs/
+(use-package skewer-mode
+  :disabled t)
+
+;; Disable this if you want to use skewer-mode
+(use-package js-mode
+  :disabled t
+  :mode "\\.json\\'")
+
+;; Load my own initialization functions
+(use-package functions
+  :config
+  (setq ring-bell-function 'echo-area-bell))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Load the rest of the site-specific configuration
 
-(let* ((system-name-short (car (split-string system-name "\\.")))
-       (site-path (concat dotfiles-dir "site/"))
-       (file-name (concat site-path system-name-short "-postload")))
-  ;; fail silently
-  (load file-name 'noerror 'nomessage))
+;; See README in ~/.emacs.d/site/
+(load (concat system-name "-postload") 'noerror)
 
 (provide 'init)
 ;;; init.el ends here
