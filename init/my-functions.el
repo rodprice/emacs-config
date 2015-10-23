654d68f914bb723ceb87d53eae208f7985b1fb10
Rodney Price <rodprice@raytheon.com>
2015-10-23 09:29:28 -0600

    Replace objc-mode with wolfram-mode in auto-mode-alist

--- init/my-functions.el
+++ init/my-functions.el
@@ -2,20 +2,6 @@
 ;;; Commentary:
 ;;; Code:
 
-(defun remove-all-matches-from-alist (name alist)
-  "Remove any entries matching NAME from ALIST.
-
-This function assumes that ALIST has the same form as
-`auto-mode-alist' and friends.  This function recurses until all
-entries matching NAME are removed.  The return value is the alist
-with matching entries removed."
-  (let ((mode (assoc-default name alist 'string-match)))
-    (if mode
-        (remove-all-matches-from-alist
-         name
-         (remove (rassoc mode alist) alist))
-      alist)))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Use git grep for the grep command. This code is adapted from `vc-git-grep'.
 
--- mode/my-wolfram.el
+++ mode/my-wolfram.el
@@ -2,6 +2,25 @@
 ;;; Commentary:
 ;;; Code:
 
+(defun remove-all-matches-from-alist (name alist)
+  "Remove any entries matching NAME from ALIST.
+
+This function assumes that ALIST has the same form as
+`auto-mode-alist' and friends.  This function recurses until all
+entries matching NAME are removed.  The return value is the alist
+with matching entries removed."
+  (let ((mode (assoc-default name alist 'string-match)))
+    (if mode
+        (remove-all-matches-from-alist
+         name
+         (remove (rassoc mode alist) alist))
+      alist)))
+
+;; Remove binding for .m files to objc-mode; bind to wolfram-mode
+(setq auto-mode-alist
+ (cons '("\\.m\\'" . wolfram-mode)
+       (remove-all-matches-from-alist ".m" auto-mode-alist)))
+
 ;; Auto-pair comment brackets
 (sp-local-pair '(wolfram-mode) "(*" "*)"
                :unless '(sp-in-string-p)
