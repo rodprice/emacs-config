9116024cb8dbc8ff62dca4cebe489846e0f9421a
Rodney Price <rod@thirdoption.info>
2015-10-30 19:04:49 -0600

    Add empty java-mode configuration file

---
+++ mode/my-java.el
@@ -0,0 +1,8 @@
+;;; my-java.el --- My Java programming configuration
+;;; Commentary:
+;; TODO: try out eclim
+;; URL: https://github.com/senny/emacs-eclim
+;;; Code:
+
+(provide 'my-java)
+;;; my-java.el ends here
