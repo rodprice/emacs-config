536e11390d0b763b6052bb0ba7df40683f43b116
Rodney Price <rodprice@raytheon.com>
2016-02-10 13:17:58 -0700

    Add function (as yet incomplete) to access pydoc from Emacs

--- mode/my-python.el
+++ mode/my-python.el
@@ -185,5 +185,32 @@ invoked from a Python process, it will switch back to the `python-mode' buffer."
             'org-display-inline-images 'append)
   :pin local)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Read documentation within Emacs
+
+;; From http://ubuntuforums.org/showthread.php?t=1363999
+(defun pydoc (w)
+  "Launch PyDOC on the Word at Point"
+  (interactive
+   (list (let* ((word (thing-at-point 'word))
+                (input (read-string
+                        (format "pydoc entry%s: " 
+                                (if (not word) "" 
+                                  (format " (default %s)" word))))))
+           (if (string= input "") 
+               (if (not word) (error "No pydoc args given")
+                 word) ;sinon word
+             input)))) ;sinon input
+  (save-window-excursion
+    (shell-command (concat "pydoc " w) "*PYDOCS*"))
+  ;;(view-buffer-other-window "*PYDOCS*" nil 'bury-buffer))
+  (view-buffer "*PYDOCS*" 'bury-buffer))
+
+;; From https://github.com/wilfred/python-info
+;; See also https://www.gnu.org/software/emacs/manual/html_node/efaq/Installing-Texinfo-documentation.html
+(add-to-list 'Info-default-directory-list
+             (expand-file-name "info/python" user-emacs-directory))
+
+
 (provide 'my-python)
 ;;; my-python.el ends here
