70cb732f8ee1d864287d970f236ba2e69d18ec54
Rodney Price <rod@thirdoption.info>
2016-02-15 21:48:25 -0700

    Try, try again at Python info docs

--- mode/my-python.el
+++ mode/my-python.el
@@ -192,23 +192,22 @@ invoked from a Python process, it will switch back to the `python-mode' buffer."
 (defun pydoc (w)
   "Launch PyDOC on the Word at Point"
   (interactive
-   (list (let* ((word (thing-at-point 'word))
-                (input (read-string
-                        (format "pydoc entry%s: " 
-                                (if (not word) "" 
-                                  (format " (default %s)" word))))))
-           (if (string= input "") 
-               (if (not word) (error "No pydoc args given")
-                 word) ;sinon word
-             input)))) ;sinon input
+   (list
+    (let* ((word (thing-at-point 'word))
+           (input (read-string
+                   (format "pydoc entry%s: " 
+                           (if (not word) "" (format " (default %s)" word))))))
+      (if (string= input "")
+          (if (not word) (error "No pydoc args given") word)
+        input))))
   (save-window-excursion
     (shell-command (concat "pydoc " w) "*PYDOCS*"))
   ;;(view-buffer-other-window "*PYDOCS*" nil 'bury-buffer))
-  (view-buffer "*PYDOCS*" 'bury-buffer))
+  (view-buffer "*PYDOCS*" 'kill-buffer-if-not-modified))
 
 ;; From https://github.com/wilfred/python-info
 ;; See also https://www.gnu.org/software/emacs/manual/html_node/efaq/Installing-Texinfo-documentation.html
-(add-to-list 'Info-default-directory-list
+(add-to-list 'Info-additional-directory-list
              (expand-file-name "info/python" user-emacs-directory))
 
 
