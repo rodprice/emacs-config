98bc879e8acbc2dc1955965a663d6c558b84f7bf
Rodney Price <price-rodney@zai.com>
2023-06-09 16:58:51 -0600

    Various cleanups

--- lisp/my-functions.el
+++ lisp/my-functions.el
@@ -915,66 +915,6 @@ that frame."
         (unless (cdr (assq 'inhibit-switch-frame alist))
           (window--maybe-raise-frame (window-frame window)))))))
 
-
-;; (defun make-new-frame (frame-name)
-;;   (make-frame `((name . ,frame-name) (width . 80) (height . 20) (fullscreen . nil)))
-;;   (select-frame-by-name frame-name))
-
-;; (defun switch-to-frame ()
-;;   (interactive)
-;;   (let ((frame-name "pytest")
-;;         (frames (frame-list)))
-;;     (catch 'break
-;;       (dolist (frame frames (make-new-frame frame-name))
-;;         (if (equal (frame-parameter frame 'name) frame-name)
-;;             (throw 'break (select-frame-set-input-focus frame)))))))
-
-
-;; (define-key ctl-x-map "\C-i"
-;;   #'endless/ispell-word-then-abbrev)
-
-;; Seems like you have to have ispell set up for this to work
-;; http://endlessparentheses.com/ispell-and-abbrev-the-perfect-auto-correct.html
-;; (defun endless/simple-get-word ()
-;;   (car-safe (save-excursion (ispell-get-word nil))))
-
-;; (defun endless/ispell-word-then-abbrev (p)
-;;   "Call `ispell-word', then create an abbrev for it.
-;; With prefix P, create local abbrev. Otherwise it will
-;; be global.
-;; If there's nothing wrong with the word at point, keep
-;; looking for a typo until the beginning of buffer. You can
-;; skip typos you don't want to fix with `SPC', and you can
-;; abort completely with `C-g'."
-;;   (interactive "P")
-;;   (let (bef aft)
-;;     (save-excursion
-;;       (while (if (setq bef (endless/simple-get-word))
-;;                  ;; Word was corrected or used quit.
-;;                  (if (ispell-word nil 'quiet)
-;;                      nil ; End the loop.
-;;                    ;; Also end if we reach `bob'.
-;;                    (not (bobp)))
-;;                ;; If there's no word at point, keep looking
-;;                ;; until `bob'.
-;;                (not (bobp)))
-;;         (backward-word)
-;;         (backward-char))
-;;       (setq aft (endless/simple-get-word)))
-;;     (if (and aft bef (not (equal aft bef)))
-;;         (let ((aft (downcase aft))
-;;               (bef (downcase bef)))
-;;           (define-abbrev
-;;             (if p local-abbrev-table global-abbrev-table)
-;;             bef aft)
-;;           (message "\"%s\" now expands to \"%s\" %sally"
-;;                    bef aft (if p "loc" "glob")))
-;;       (user-error "No typo at or before point"))))
-
-;; (setq save-abbrevs 'silently)
-;; (setq-default abbrev-mode t)
-
-
 (defvar regexp-frame-names "^\\(?:MAIN\\|SYSTEM\\|ORG\\|MISCELLANEOUS\\|COMPILATION\\)$"
     "Regexp matching frames with specific names.")
 
