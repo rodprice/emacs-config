13680029daaa1ad14d0fea97ef36eef76094a77f
Rodney Price <price-rodney@zai.com>
2023-06-14 18:22:39 -0600

    Install emacs-jupyter

---
+++ elpa/jupyter-20230608.1504/jupyter-pkg.el
@@ -0,0 +1,16 @@
+(define-package "jupyter" "20230608.1504" "Jupyter"
+  '((emacs "26")
+    (cl-lib "0.5")
+    (zmq "0.10.10")
+    (simple-httpd "1.5.0")
+    (websocket "1.9"))
+  :commit "1fb77da2d394b905f9dfa325a9d6ef9e2cf52541" :authors
+  '(("Nathaniel Nicandro" . "nathanielnicandro@gmail.com"))
+  :maintainers
+  '(("Nathaniel Nicandro" . "nathanielnicandro@gmail.com"))
+  :maintainer
+  '("Nathaniel Nicandro" . "nathanielnicandro@gmail.com")
+  :url "https://github.com/nnicandro/emacs-jupyter")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
